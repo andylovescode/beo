13e7444ba639d2c0965280b7e255a6ecbcbb5073
andylovescode <andytk2010@gmail.com>
2026-04-06 11:44:44 -0700

    version+meta: prepare for automatic version releases

--- version/release.ts
+++ version/release.ts
@@ -3,6 +3,13 @@ import { readChangelog, writeChangelog } from "./change.ts"
 export async function release() {
 	const changes = await readChangelog()
 
+	const latestChange = changes.at(-1)
+
+	if (latestChange && latestChange.type === "release") {
+		console.log("[info]: a release just happened, not doing anything")
+		Deno.exit(0)
+	}
+
 	changes.push({
 		type: "release",
 	})
