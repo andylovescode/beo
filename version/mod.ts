2845807f64ae068c1bf54a34ff70408bccdaa1d5
andylovescode <andytk2010@gmail.com>
2026-04-05 22:37:12 -0700

    version: add description

--- version/mod.ts
+++ version/mod.ts
@@ -1,3 +1,15 @@
+/**
+ * @module
+ *
+ * # @beo/version
+ *
+ * This is a minimal version manager
+ *
+ * - initializing a repo: glhf
+ * - releasing: deno run jsr:@beo/version --release
+ * - adding a change: deno run jsr:@beo/version --add
+ */
+
 import { sync } from "./sync.ts"
 import { parseArgs } from "@std/cli"
 import { add } from "./add.ts"
