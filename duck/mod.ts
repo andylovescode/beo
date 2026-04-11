c2a87b9b382d11ab15e6b780b03dee52fe1256a7
andylovescode <andytk2010@gmail.com>
2026-04-11 15:06:06 -0700

    duck: document

--- duck/mod.ts
+++ duck/mod.ts
@@ -11,12 +11,16 @@
  * @module duck
  */
 
-import type { Signal } from "@beo/signal"
-
 type TrueBooleanKeys<T> = keyof ({
 	[k in keyof T]: T[k] extends true ? { [x in k]: true } : never
 })[keyof T]
 
+/**
+ * Validates the type of an object
+ * @param it The object to check the type of
+ * @param key A true boolean key only in `T`
+ * @returns true, if `it` is the desired type
+ */
 export function is<T>(it: unknown, key: TrueBooleanKeys<T>): it is T {
 	return (typeof it === "object" && it && key in it &&
 			(it as any)[key] === true)
